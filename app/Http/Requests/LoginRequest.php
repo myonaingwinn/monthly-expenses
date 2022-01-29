3d1332d9867b134a0da58e1b89354622e46ed143
myonaingwinn <myonaingwin.gic@gmail.com>
2022-01-29 18:09:45 +0630

    Login Ok

---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'email' => 'required',
+            'password' => 'required'
+        ];
+    }
+
+    /**
+     * Get the needed authorization credentials from the request.
+     *
+     * @return array
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     */
+    public function getCredentials()
+    {
+        $email = $this->get('email');
+
+        if ($email) {
+            return [
+                'email' => $email,
+                'password' => $this->get('password')
+            ];
+        }
+
+        return $this->only('email', 'password');
+    }
+}
